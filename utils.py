b3e8eb126840207b5ec9ab67c8935074c3a51858
Luc <99895019+Luckyluuuc@users.noreply.github.com>
2023-09-28 10:23:10 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,52 @@
+def tokenize_text(text):
+    # Use a regular expression to tokenize the text into words while preserving spaces and line breaks
+    tokens =  re.findall(r'\w+|[\.,;!?"]+|\n|\t|.', text)
+    return tokens
+
+def mapp(text): # return 2 dictionnaries mapping each token to an integers and vice versa
+  list_token = sorted(list(set(text)))
+    vocab_size = len(list_token)
+  
+  
+
+def encode(text, stoi = stoi):
+  list_integers = []
+  for c in text:
+    list_integers.append(stoi.get(c))
+
+  return list_integers
+
+
+def decode(list_integers, itos=itos):
+  text = []
+  for i in list_integers:
+    text.append(itos.get(i))
+
+  text = ''.join(c for c in text) #delete this line if you want a list of char instead of a str
+  return text
+
+def get_batch(split, block_size, batch_size): #split is either "train" or "eval"
+  assert split in ["train", "eval"], "split must be 'train' or 'eval'"
+  data = train_set if split == "train" else validation_set
+
+  ix = torch.randint(0, len(data) - block_size-1, (batch_size,)) # return a tensor of shape (batch_size) with random values bitween 0 and len(data) - block_size
+
+  x = torch.stack([torch.tensor(data[i:i + block_size]) for i in ix])
+  y = torch.stack([torch.tensor(data[i + 1:i + block_size + 1]) for i in ix])
+
+  x, y = x.to(device), y.to(device)
+  return x, y
+
+@torch.no_grad()
+def estimate_loss(m, eval_iters):
+  out = {}
+  m.eval()
+  for split in ["train", "eval"]:
+    losses = torch.zeros(eval_iters)
+    for k in range(eval_iters):
+      X, Y = get_batch(split, cfg.get('block_size'), cfg.get('batch_size'), cfg.get("device"))
+      logits, loss = m(X, Y)
+      losses[k] = loss.item()
+    out[split] = losses.mean()
+  m.train()
+  return out
